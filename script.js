c21ab5d9b6716e5dc39cd1862216b84b4189ddc5
Miguel Fiais <miguel.fiais10@gmail.com>
2022-09-07 17:55:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const iconMenu = document.getElementById('icon-menu');
+const navList = document.querySelector('ul')
+iconMenu.addEventListener('click', () =>{
+    if(navList.style.maxHeight==="0px"){
+        navList.style.maxHeight="50vh"
+        iconMenu.src = "./assets/x_icon_172101.svg"
+    }
+    else{
+        navList.style.maxHeight="0px"
+        iconMenu.src="./assets/icon-menu.svg"
+    }
+})
