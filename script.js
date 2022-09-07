3532c5bb62e926ba1f6ab47a2e8f5afa15008ae9
Miguel Fiais <miguel.fiais10@gmail.com>
2022-09-07 18:08:52 -0300

    ajustes menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const iconMenu = document.getElementById('icon-menu');
 const navList = document.querySelector('ul')
 iconMenu.addEventListener('click', () =>{
     if(navList.style.maxHeight==="0px"){
-        navList.style.maxHeight="50vh"
+        navList.style.maxHeight="70vh"
         iconMenu.src = "./assets/x_icon_172101.svg"
     }
     else{
